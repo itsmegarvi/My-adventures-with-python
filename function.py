69d99847c29593a06d43d4521575ce16983d7c1a
Garvit Shubham <70941313+itsmegarvi@users.noreply.github.com>
2020-09-09 00:37:57 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,10 @@
+def greet(lang):
+    if lang == 'es':
+        print ("ola")
+    elif lang == 'fr':
+        print ("Bonjour")
+    else:
+        print ("Hello")
+greet('en')
+greet('es')
+greet('fr')
