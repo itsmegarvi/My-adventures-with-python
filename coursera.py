8d9d3d857356cc397013fe1359225d99d0fc3187
Garvit Shubham <70941313+itsmegarvi@users.noreply.github.com>
2020-09-09 02:08:44 +0530

    Update coursera.py

--- coursera.py
+++ coursera.py
@@ -2,7 +2,7 @@ x = input("Enter hours: ")
 y = input('Enter rate: ')
 z = float(x) * float(y)
 a = (float(x)- 40) * 1.5 * float(y) + 40*float(y)
-if float(x) < 40:
+if float(x) <= 40:
     print (z)
 elif float(x) > 40:
     print (z)
