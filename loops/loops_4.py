3673d271df9052c81530614a5fdadc9e6956abde
Garvit Shubham <mailme.garvit@gmail.com>
2020-09-24 01:56:25 +0530

    loops

--- loops/loops_4.py
+++ loops/loops_4.py
@@ -1,1 +1,9 @@
-# QUESTION: Display Fibonacci series up to 10 terms.
+# QUESTION: Write a code to extract each digit from an integer, in the reverse order.
+
+def reverseint(num):
+    while num != 0:
+        print (num % 10 , end = " ")
+        num //= 10
+
+
+reverseint(6457)
